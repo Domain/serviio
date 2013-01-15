20e861625042d76c8f7c264453c7b7bb2624f5f7
Domain <do.main@msn.com>
2013-01-15 22:15:58 +0800

    remove TestResult in gitignore

---
+++ external/net/padlocksoftware/padlock/license/TestResult.d
@@ -0,0 +1,12 @@
+/// Generate by tools
+module net.padlocksoftware.padlock.license.TestResult;
+
+import java.lang.exceptions;
+
+public class TestResult
+{
+    public this()
+    {
+        implMissing();
+    }
+}
